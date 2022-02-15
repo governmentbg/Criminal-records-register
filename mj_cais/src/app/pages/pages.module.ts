3af1d843f8dd3535b58d2f0d5d15cba06e1c6b4e
Milen Donchev <mdonchev@technologica.com>
2022-02-15 16:46:49 +0200

    Added tabs to bulletin form

--- mj_cais/src/app/pages/pages.module.ts
+++ mj_cais/src/app/pages/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { NbMenuModule } from "@nebular/theme";
+import { NbMenuModule, NbTabsetModule } from "@nebular/theme";
 
 import { ThemeModule } from "../@theme/theme.module";
 import { PagesComponent } from "./pages.component";
@@ -18,6 +18,7 @@ import { CoreModule } from "../@core/core.module";
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
+    NbTabsetModule,
     DashboardModule,
     MiscellaneousModule,
     SharedModule,
