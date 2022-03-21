b55a70111dd7b039f7579831badf7654008da0e5
Milen Donchev <mdonchev@technologica.com>
2022-03-21 17:38:59 +0200

    Added person validator client for Canko's api

---
+++ MJ_CAIS.API/Tools/MJ_CAIS.PersonValidator/MJ_CAIS.PersonValidator/PersonInfoRequest.cs
@@ -0,0 +1,34 @@
+﻿namespace MJ_CAIS.PersonValidator
+{
+    public class PersonInfoRequest
+    {
+        public string year { get; set; }
+        public string month { get; set; }
+        public string day { get; set; }
+        public string gender { get; set; }
+        public string fname { get; set; }
+        public string sname { get; set; }
+        public string lname { get; set; }
+        public string threshold { get; set; }
+
+        public Dictionary<string, string?> GetKeyValuePairs()
+        {
+            var properties = this.GetType().GetProperties();
+            var result = new Dictionary<string, string?>();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(this)?.ToString();
+                result.Add(property.Name, value);
+            }
+
+            return result;
+        }
+    }
+
+    public enum GenderType
+    {
+        male,
+        female,
+    }
+}
