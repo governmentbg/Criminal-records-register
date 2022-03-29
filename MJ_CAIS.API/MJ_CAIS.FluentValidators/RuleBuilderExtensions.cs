0906058b039762ad7c04c0a61ce4be871bfb1a0a
Stefka Vasileva <svasileva@technologica.com>
2022-03-29 17:26:19 +0300

    Добавени валидации за дължина на полетата #44404

--- MJ_CAIS.API/MJ_CAIS.FluentValidators/RuleBuilderExtensions.cs
+++ MJ_CAIS.API/MJ_CAIS.FluentValidators/RuleBuilderExtensions.cs
@@ -6,7 +6,33 @@ namespace MJ_CAIS.FluentValidators
     {
         public static IRuleBuilderOptions<T, TProperty> RequredField<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder)
         {
-            return ruleBuilder.NotEmpty().WithMessage(ValidationMessageConstants.NotEmpty);
+            return ruleBuilder
+                .NotEmpty()
+                .WithMessage(ValidationMessageConstants.NotEmpty);
+        }
+
+        public static IRuleBuilderOptions<T, string?> HasMaxLength<T>(this IRuleBuilder<T, string?> ruleBuilder, int maxLength)
+        {
+            var message = string.Format(ValidationMessageConstants.HasMaxLength, maxLength);
+            return ruleBuilder
+                .Length(0, 100)
+                .WithMessage(message);
+        }
+
+        public static IRuleBuilderOptions<T, string?> HasMinLength<T>(this IRuleBuilder<T, string?> ruleBuilder, int minLength)
+        {
+            var message = string.Format(ValidationMessageConstants.HasMinLength, minLength);
+            return ruleBuilder
+                .Length(minLength, int.MaxValue)
+                .WithMessage(message);
+        }
+
+        public static IRuleBuilderOptions<T, string?> HasLength<T>(this IRuleBuilder<T, string?> ruleBuilder, int minLength, int maxLength)
+        {
+            var message = string.Format(ValidationMessageConstants.HasLength, maxLength, minLength);
+            return ruleBuilder
+                .Length(minLength, maxLength)
+                .WithMessage(message);
         }
     }
 }
--- MJ_CAIS.API/MJ_CAIS.FluentValidators/ValidationMessageConstants.cs
+++ MJ_CAIS.API/MJ_CAIS.FluentValidators/ValidationMessageConstants.cs
@@ -3,5 +3,8 @@
     public static class ValidationMessageConstants
     {
         public static string NotEmpty = "Полето е задължително";
+        public static string HasMaxLength = "Очакват се най-много {0} символа";
+        public static string HasMinLength = "Очакват се най-малко {0} символа";
+        public static string HasLength = "Очакват се най-много {0} и по-малко {1} символа";
     }
 }
