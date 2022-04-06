145121b25961201b12cfada4cdacc452fe594d05
Milen Donchev <mdonchev@technologica.com>
2022-04-06 11:41:08 +0300

    Extract data to partial, change types

--- MJ_CAIS.API/MJ_CAIS.DataAccess/Partials/EEcrisMessagePartial.cs
+++ MJ_CAIS.API/MJ_CAIS.DataAccess/Partials/EEcrisMessagePartial.cs
@@ -1,38 +1,14 @@
-﻿using System.Globalization;
+﻿using MJ_CAIS.Common.XmlData;
+using System.Globalization;
 using System.Xml;
 
-namespace MJ_CAIS.Common.XmlData
+namespace MJ_CAIS.DataAccess.Entities
 {
-    public class EcrisMessageXmlDTO
+    public partial class EEcrisMessage
     {
-        public string? RequestMsgId { get; set; }
-        public string? FromAuthId { get; set; }
-        public string? ToAuthId { get; set; }
-        public string? Identifier { get; set; }
-        public string? EcrisIdentifier { get; set; }
-        public DateTime? MsgTimestamp { get; set; }
-        public string? ResponseTypeId { get; set; }
-        public string? EcrisMsgStatus { get; set; }
-        public DateTime? BirthDate { get; set; }
-        public string? BirthCountry { get; set; }
-        public string? BirthCity { get; set; }
-        public string? FbbcId { get; set; }
-        public string? Firstname { get; set; }
-        public string? Surname { get; set; }
-        public string? Familyname { get; set; }
-        public decimal? Sex { get; set; }
-        public string? Nationality1Code { get; set; }
-        public string? Nationality2Code { get; set; }
-        public string? MsgTypeId { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime? CreatedOn { get; set; }
-        public string? UpdatedBy { get; set; }
-        public DateTime? UpdatedOn { get; set; }
-        public string? EcrisMsgConvictionId { get; set; }
-
-        public static EcrisMessageXmlDTO ParseXml(string xml)
+        public static EEcrisMessage ParseXml(string xml)
         {
-            var result = new EcrisMessageXmlDTO();
+            var result = new EEcrisMessage();
             var doc = new XmlDocument();
             doc.LoadXml(xml);
             var root = doc.DocumentElement;
