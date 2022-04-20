9f00603aa662c307bdcce6d590e654a1f4b6f8cc
nzheleva <nzheleva@technologica.com>
2022-04-20 13:28:28 +0300

    Integration service for EISS, and ConvictionStatusReports

---
+++ MJ_CAIS.API/Tools/ExternalServicesHost/ExternalServicesHost/EISSIntegrationService.cs
@@ -0,0 +1,19 @@
+﻿using MJ_CAIS.BulletinModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MJ_CAIS.ExternalServicesHost
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class EISSIntegrationService : IEISSIntegrationService
+    {
+        public void SendData(SendDataRequestType value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
