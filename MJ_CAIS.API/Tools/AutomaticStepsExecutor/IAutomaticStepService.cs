4a503872234864ae0c8ce264fdafabcccb2650e2
Kostadinka Toteva <kossy.toteva@gmail.com>
2022-05-16 16:18:16 +0300

    #44908 - тул за автоматично изпълнение

---
+++ MJ_CAIS.API/Tools/AutomaticStepsExecutor/IAutomaticStepService.cs
@@ -0,0 +1,21 @@
+﻿using MJ_CAIS.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticStepsExecutor
+{
+    public interface IAutomaticStepService
+    {
+        public  Task PreSelectAsync();      
+        public  Task<List<BaseEntity>> SelectEntitiesAsync();
+        public  Task PostSelectAsync();
+        public  Task PreProcessAsync();
+        public  Task<AutomaticStepResult> ProcessEntitiesAsync(List<BaseEntity> entities);
+        public  Task PostProcessAsync();
+
+
+    }
+}
