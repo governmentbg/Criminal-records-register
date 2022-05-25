86ff59a2e1841242c6d5daf0136c709058264e80
Stefka Vasileva <svasileva@technologica.com>
2022-05-25 10:55:22 +0300

    Актуализация на бюлетин през бутон от грида

--- mj_cais/src/app/pages/bulletin/bulletin-overview/bulletin-newoffice-overview/bulletin-newoffice-overview.component.ts
+++ mj_cais/src/app/pages/bulletin/bulletin-overview/bulletin-newoffice-overview/bulletin-newoffice-overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, Injector } from "@angular/core";
+import { NbDialogService } from "@nebular/theme";
+import { TranslateService } from "@ngx-translate/core";
+import { ConfirmDialogComponent } from "../../../../@core/components/dialogs/confirm-dialog-component/confirm-dialog-component.component";
+import { CommonConstants } from "../../../../@core/constants/common.constants";
 import { RemoteGridWithStatePersistance } from "../../../../@core/directives/remote-grid-with-state-persistance.directive";
 import { DateFormatService } from "../../../../@core/services/common/date-format.service";
 import { BulletinGridService } from "../_data/bulletin-grid.service";
@@ -14,12 +18,56 @@ export class BulletinNewOfficeOverviewComponent extends RemoteGridWithStatePersi
   BulletinGridModel,
   BulletinGridService
 > {
-  constructor(service: BulletinGridService, injector: Injector, public dateFormatService: DateFormatService) {
+  constructor(
+    service: BulletinGridService,
+    injector: Injector,
+    public dateFormatService: DateFormatService,
+    private dialogService: NbDialogService,
+    private translate: TranslateService
+  ) {
     super("bulletins-search", service, injector);
     this.service.updateUrlStatus(BulletinStatusTypeEnum.NewOffice);
   }
 
   ngOnInit() {
     super.ngOnInit();
+  }
+
+  public openUpdateConfirmationDialog(bulletinId: string) {
+    let dialogRef = this.dialogService.open(
+      ConfirmDialogComponent,
+      CommonConstants.defaultDialogConfig
+    );
+
+    dialogRef.componentRef.instance.confirmMessage = this.translate.instant(
+      "BULLETIN.CONFIRM-MESSAGE-WHEN-UPDATE"
+    );
+    dialogRef.componentRef.instance.showHeder = false;
+
+    dialogRef.onClose.subscribe((result) => {
+      if (result) {
+        this.service
+          .changeStatus(bulletinId, BulletinStatusTypeEnum.Active)
+          .subscribe(
+            (res) => {
+              this.toastr.showToast(
+                "success",
+                this.translate.instant("BULLETIN.SUCCESS-UPDATE-STATUS")
+              );
+              this.router.navigate(["pages/bulletins"]);
+            },
+            (error) => {
+              let title = this.dangerMessage;
+              let errorText = error.status + " " + error.statusText;
+              if (error.error && error.error.customMessage) {
+                title = error.error.customMessage;
+                errorText = "";
+              }
+
+              this.toastr.showBodyToast("danger", title, errorText);
+            }
+          );
+      }
+    });
   }
 }
