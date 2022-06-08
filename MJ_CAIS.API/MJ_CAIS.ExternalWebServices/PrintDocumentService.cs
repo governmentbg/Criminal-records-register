d7a4e5b4d304c0849e8d01786ac52a2864ada3b4
Milen Donchev <mdonchev@technologica.com>
2022-06-08 13:20:22 +0300

    Remove thread blocking .Result

--- MJ_CAIS.API/MJ_CAIS.ExternalWebServices/PrintDocumentService.cs
+++ MJ_CAIS.API/MJ_CAIS.ExternalWebServices/PrintDocumentService.cs
@@ -19,43 +19,40 @@ namespace MJ_CAIS.ExternalWebServices
 {
     public class PrintDocumentService : IPrintDocumentService
     {
-        //private readonly ICertificateRepository _certificateRepository;
         private readonly IJasperReportsClient _jasperReportsClient;
  
-
-        public PrintDocumentService(IJasperReportsClient jasperClient) 
-
+        public PrintDocumentService(IJasperReportsClient jasperClient)
         {          
             _jasperReportsClient = jasperClient;
         }
 
         public async Task<byte[]> PrintApplication(string applicationID)
         {
-            Dictionary<string, string> inputs = new Dictionary<string, string> { { "Application_ID", applicationID } };
-            byte[] fileArray = await Task.FromResult(_jasperReportsClient.RunReportBuffered(GetUrlOfCertificateReport(JasperReportsNames.Application_Report), OutputFormats.pdf, inputs).Result);
+            var inputs = new Dictionary<string, string> { { "Application_ID", applicationID } };
+            var pathToReport = GetUrlOfCertificateReport(JasperReportsNames.Application_Report);
+            var fileArray = await _jasperReportsClient.RunReportBuffered(pathToReport, OutputFormats.pdf, inputs);
             return fileArray;
-
         }
 
         public async Task<byte[]> PrintCertificate(string certificateID, string checkUrl, JasperReportsNames reportName)
         {
-            Dictionary<string, string> inputs = new Dictionary<string, string> { { "certificate_id", certificateID }, { "check_url", checkUrl } };
-            byte[] fileArray = await Task.FromResult(_jasperReportsClient.RunReportBuffered(GetUrlOfCertificateReport(reportName), OutputFormats.pdf, inputs).Result);
+            var inputs = new Dictionary<string, string> { { "certificate_id", certificateID }, { "check_url", checkUrl } };
+            var pathToReport = GetUrlOfCertificateReport(reportName);
+            var fileArray = await _jasperReportsClient.RunReportBuffered(pathToReport, OutputFormats.pdf, inputs);
             return fileArray;
         }
 
         public async Task<byte[]> PrintReport(string reportId,  JasperReportsNames reportName)
         {
-            Dictionary<string, string> inputs = new Dictionary<string, string> { { "report_id", reportId }};
-            byte[] fileArray = await Task.FromResult(_jasperReportsClient.RunReportBuffered(GetUrlOfCertificateReport(reportName), OutputFormats.pdf, inputs).Result);
+            var inputs = new Dictionary<string, string> { { "report_id", reportId }};
+            var pathToReport = GetUrlOfCertificateReport(reportName);
+            var fileArray = await _jasperReportsClient.RunReportBuffered(pathToReport, OutputFormats.pdf, inputs);
             return fileArray;
         }
 
         private string GetUrlOfCertificateReport(JasperReportsNames reportName)
         {
-
             return $"{CertificateConstants.UrlsInJasper.REPORTS_URL}/{reportName}";
         }
-
     }
 }
