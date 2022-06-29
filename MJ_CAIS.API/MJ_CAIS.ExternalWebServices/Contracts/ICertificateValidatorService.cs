7790b6f08dae94855f31df9b2b439cfb509afafe
Kostadinka Toteva <kossy.toteva@gmail.com>
2022-06-29 16:03:47 +0300

    bugfix

--- MJ_CAIS.API/MJ_CAIS.ExternalWebServices/Contracts/ICertificateValidatorService.cs
+++ MJ_CAIS.API/MJ_CAIS.ExternalWebServices/Contracts/ICertificateValidatorService.cs
@@ -3,5 +3,7 @@
     public interface ICertificateValidatorService
     {
        Task<bool> ValidatePdf(byte[] pdfBytes,  string validationString, string certificateID);
+        Task<byte[]> GetPdfForDownload(string certificateID);
+        Task<bool> ValidatePdf(byte[] pdfBytes, string certificateID);
     }
 }
