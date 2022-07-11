801384def9fc4d4114622c44a1d323ff97da6826
Stefka Vasileva <svasileva@technologica.com>
2022-07-11 13:09:46 +0300

    trim string value in cais

---
+++ MJ_CAIS.API/MJ_CAIS.Web/Utils/TrimStringJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MJ_CAIS.Web.Utils
+{
+    public class TrimStringJsonConverter : JsonConverter<string?>
+    {
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetString().Trim();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+    }
+}
