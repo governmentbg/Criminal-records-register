84a23d2c72ee45063bb8b9ff21f9992645fa8869
Kostadinka Toteva <kossy.toteva@gmail.com>
2022-07-13 14:25:50 +0300

    промяна в извикването на Jasper Reports

--- MJ_CAIS.API/MJ_CAIS.Common/Enums/JasperReportsNames.cs
+++ MJ_CAIS.API/MJ_CAIS.Common/Enums/JasperReportsNames.cs
@@ -8,13 +8,16 @@ namespace MJ_CAIS.Common.Enums
 {
     public enum JasperReportsNames
     {
-        Certificate_with_conviction,
-        Certificate_without_conviction,
+        Certificate,
+        //Certificate_with_conviction,
+        //Certificate_without_conviction,
         Application_Report,
         Conviction_Report,
-        Electronic_certificate_without_conviction,
-        Electronic_certificate_with_conviction,
-        Electronic_external_certificate_with_conviction,
-        Electronic_external_certificate_without_conviction
+        Electronic_certificate,
+        Electronic_external_certificate
+        //Electronic_certificate_without_conviction,
+        //Electronic_certificate_with_conviction,
+        //Electronic_external_certificate_with_conviction,
+        //Electronic_external_certificate_without_conviction
     }
 }
