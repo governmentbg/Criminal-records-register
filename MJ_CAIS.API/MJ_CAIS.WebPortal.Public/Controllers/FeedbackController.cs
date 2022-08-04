cdc3730e897b6ce7259b9af365a5eb2d32d6f9df
Dimitar Mitev <dmitev@technologica.com>
2022-08-04 11:52:24 +0300

    Added system parameters for feedback related settings; changed menu alignment

--- MJ_CAIS.API/MJ_CAIS.WebPortal.Public/Controllers/FeedbackController.cs
+++ MJ_CAIS.API/MJ_CAIS.WebPortal.Public/Controllers/FeedbackController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MJ_CAIS.Common.Resources;
 using MJ_CAIS.Services.Contracts;
 using MJ_CAIS.WebPortal.Public.Models.Feedback;
 using System.Text.Encodings.Web;
@@ -8,9 +9,16 @@ namespace MJ_CAIS.WebPortal.Public.Controllers
     public class FeedbackController : BaseController
     {
         private readonly IEEMailEventService _emailEventService;
-        public FeedbackController(IEEMailEventService emailEventService)
+        private readonly IGSystemParameterService _systemParameterService;
+        private readonly ILogger<FeedbackController> _logger;
+        public FeedbackController(
+            IEEMailEventService emailEventService,
+            IGSystemParameterService systemParameterService,
+            ILogger<FeedbackController> logger)
         {
             _emailEventService = emailEventService;
+            _systemParameterService = systemParameterService;
+            _logger = logger;
         }
         [HttpGet]
         public ActionResult Index()
@@ -22,19 +30,30 @@ namespace MJ_CAIS.WebPortal.Public.Controllers
         }
 
         [HttpPost]
-        public ActionResult Send(FeedbackViewModel viewModel,[FromServices]  HtmlEncoder htmlEncoder)
+        public async Task<ActionResult> Send(FeedbackViewModel viewModel,[FromServices]  HtmlEncoder htmlEncoder)
         {
             var name = htmlEncoder.Encode(viewModel.Name ?? "");
+            var email = htmlEncoder.Encode(viewModel.Email ?? "");
             var feedbackForId = htmlEncoder.Encode(viewModel.FeedbackForId ?? "");
             var description = htmlEncoder.Encode(viewModel.Description ?? "");
-
-            _emailEventService.AddEmailEvent(
-                //TODO: Get mail group from settings
-                "dmitev@technologica.com",
-                !string.IsNullOrEmpty(name) ? 
-                $"{feedbackForId} - {name}" :
-                feedbackForId,
-                description
+            var to = await _systemParameterService.GetValueString("CRR_PUBLIC_FEEDBACK_EMAIL");
+            if (string.IsNullOrEmpty(to))
+            {
+                _logger.LogError("CRR_PUBLIC_FEEDBACK_EMAIL system varaible not set!");
+                return View("Failure");
+            }
+            var subject = await _systemParameterService.GetValueString("CRR_PUBLIC_FEEDBACK_SUBJECT");
+            if (string.IsNullOrEmpty(subject))
+            {
+                _logger.LogError("CRR_PUBLIC_FEEDBACK_SUBJECT system varaible not set!");
+                return View("Failure");
+            }
+            await _emailEventService.AddEmailEvent(
+                to,
+                subject,
+                $"{FeedbackResources.lblName}: {name}({email})<br/>" +
+                $"{FeedbackResources.lblFeedbackFor}: {feedbackForId}<br/>" +
+                $"{FeedbackResources.lblDescription}: {description}"
             );
             return View("Success");
         }
