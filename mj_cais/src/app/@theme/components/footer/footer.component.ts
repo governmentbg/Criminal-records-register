607769413b2a068cc60d705137bf3dcbab6bb53d
Iliya Deveretsov <ideveretsov@technologica.com>
2022-08-31 15:26:01 +0300

    Оправен footer.

--- mj_cais/src/app/@theme/components/footer/footer.component.ts
+++ mj_cais/src/app/@theme/components/footer/footer.component.ts
@@ -3,19 +3,13 @@ import { Component } from "@angular/core";
 @Component({
   selector: "ngx-footer",
   styleUrls: ["./footer.component.scss"],
-  template: `
-    <span class="created-by">
-      <div class="row" style="margin-right: 20px;">
-        <p class="footer-text">
-          Създаден в рамките на договор № BG05SFOP001-3.001-0010-C01/23.06.2017
-          г. по проект: <br />
-          „Реализиране на Централизирана автоматизирана информационна система
-          „Съдебен статус“, <br />
-          финансиран по Оперативна програма „Добро управление“, чрез ЕСФ. <br />
-        </p>
-        <img src="assets/images/eu.png" alt="eu-logo" class="eu-logo" />
-      </div>
-    </span>
+  template: `<div class="d-flex mt-1 flex-fill justify-content-center gap-5">
+  <p class="text-center mx-5">
+  Създаден в рамките на договор № BG05SFOP001-3.001-0010-C01/23.06.2017
+  г. по проект: „Реализиране на Централизирана автоматизирана информационна система „Съдебен статус“, <br>
+  финансиран по Оперативна програма „Добро управление“, чрез ЕСФ.
+</p>
+<img src="assets/images/eu.png" alt="eu-logo" class="eu-logo img-fluid" /></div>
   `,
 })
-export class FooterComponent {}
+export class FooterComponent { }
