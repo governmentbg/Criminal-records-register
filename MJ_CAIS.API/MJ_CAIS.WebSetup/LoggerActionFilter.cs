6af7fb1fb2ea19bc8455c28509495f78e676fa7b
Dimitar Mitev <dmitev@technologica.com>
2022-09-18 20:35:38 +0300

    Changed logging configurations. Added userId

--- MJ_CAIS.API/MJ_CAIS.WebSetup/LoggerActionFilter.cs
+++ MJ_CAIS.API/MJ_CAIS.WebSetup/LoggerActionFilter.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Controllers;
+﻿using IdentityModel;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,16 +28,18 @@ namespace MJ_CAIS.WebSetup
                 var controllerName = controllerDescriptor.ControllerName;
                 var actionName = controllerDescriptor.ActionName;
                 var identityName = context?.HttpContext?.User?.Identity?.Name;
+                var identityId = context?.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == JwtClaimTypes.Subject)?.Value;
                 var path = context?.HttpContext?.Request?.Path;
                 var queryString = context?.HttpContext?.Request?.QueryString.Value;
                 var method = context?.HttpContext?.Request?.Method;
                 var remoteIP = context?.HttpContext?.Connection?.RemoteIpAddress;
                 _logger.LogInformation(
-                    "{method} {controllerName} {actionName} {identityName} {path} {queryString}", 
+                    "{method} {controllerName} {actionName} {identityName}({identityId}) {path} {queryString} {remoteIP}", 
                     method,
                     controllerName,
                     actionName,
                     identityName,
+                    identityId,
                     path,
                     queryString,
                     remoteIP);
