4561660514d6e2a2aadce99f569bbf71c97ba0eb
Kostadinka Toteva <kossy.toteva@gmail.com>
2022-09-28 17:48:24 +0300

    тул за оправяне

--- MJ_CAIS.API/Tools/AutomaticStepsExecutor/CertificatePdfCreatorService.cs
+++ MJ_CAIS.API/Tools/AutomaticStepsExecutor/CertificatePdfCreatorService.cs
@@ -55,46 +55,93 @@ namespace AutomaticStepsExecutor
             int numberOfFailedEntities = 0;
             if (entities.Count > 0)
             {
-                var statuses = await _dbContext.AApplicationStatuses.Where(x => x.Code == ApplicationConstants.ApplicationStatuses.CertificateServerSign
-            || x.Code == ApplicationConstants.ApplicationStatuses.CertificateForDelivery
-            || x.Code == ApplicationConstants.ApplicationStatuses.CertificatePaperPrint
-            || x.Code == ApplicationConstants.ApplicationStatuses.Delivered).ToListAsync();
-                if (statuses.Count != 4)
+                //todo: направо всички за сертификат?!
+                var statuses = await Task.FromResult(_dbContext.AApplicationStatuses.AsNoTracking().Where(a =>
+                                                     a.Code == ApplicationConstants.ApplicationStatuses.BulletinsCheck
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.CertificateContentReady
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.ApprovedApplication
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.CertificateUserSign
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.CertificateForDelivery
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.CertificatePaperPrint
+                                                  || a.Code == ApplicationConstants.ApplicationStatuses.Delivered).ToList());
+                if (statuses.Count != 7)
                 {
-                    throw new Exception($"Няма въведени статуси: {ApplicationConstants.ApplicationStatuses.CertificateServerSign}, {ApplicationConstants.ApplicationStatuses.Delivered}, { ApplicationConstants.ApplicationStatuses.CertificateForDelivery}, {ApplicationConstants.ApplicationStatuses.CertificatePaperPrint}");
+                    throw new Exception($"Application statuses do not exist. Statuses: {ApplicationConstants.ApplicationStatuses.ApprovedApplication}, {ApplicationConstants.ApplicationStatuses.BulletinsCheck}, {ApplicationConstants.ApplicationStatuses.CertificateContentReady}," +
+                        $" {ApplicationConstants.ApplicationStatuses.CertificateUserSign},{ ApplicationConstants.ApplicationStatuses.CertificateForDelivery },                                                   { ApplicationConstants.ApplicationStatuses.CertificatePaperPrint}," +
+                         $"{ ApplicationConstants.ApplicationStatuses.Delivered} ");
+
                 }
-                var statusCertificateServerSign = statuses.First(s => s.Code == ApplicationConstants.ApplicationStatuses.CertificateServerSign);
+                var systemParameters = await Task.FromResult(_dbContext.GSystemParameters.AsNoTracking().Where(x => x.Code == SystemParametersConstants.SystemParametersNames.CERTIFICATE_VALIDITY_PERIOD_MONTHS
+                                                    || x.Code == SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME).ToList());
+                if (systemParameters.Count != 2)
+                {
+                    throw new Exception($"Parameters do not exist. Parameters: {SystemParametersConstants.SystemParametersNames.CERTIFICATE_VALIDITY_PERIOD_MONTHS}, {SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME}");
+                }
+                var certificateValidityMonths = systemParameters.First(x => x.Code == SystemParametersConstants.SystemParametersNames.CERTIFICATE_VALIDITY_PERIOD_MONTHS).ValueNumber;
+                if (certificateValidityMonths == null)
+                {
+                    throw new Exception($"System parameter {SystemParametersConstants.SystemParametersNames.CERTIFICATE_VALIDITY_PERIOD_MONTHS} not set.");
+                }
+                var signingCertificateName = systemParameters.First(x => x.Code == SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME).ValueString;
+                if (signingCertificateName == null)
+                {
+                    throw new Exception($"System parameter {SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME} not set.");
+                }
+                var applicationStatus = statuses.First(a => a.Code == ApplicationConstants.ApplicationStatuses.ApprovedApplication);
+                var certificateContentReadyStatus = statuses.First(a => a.Code == ApplicationConstants.ApplicationStatuses.CertificateContentReady);
+                var bulletinCheckStatus = statuses.First(a => a.Code == ApplicationConstants.ApplicationStatuses.BulletinsCheck);
+                var statusCertificateUserSign = statuses.First(s => s.Code == ApplicationConstants.ApplicationStatuses.CertificateUserSign);
                 var statusForDelivery = statuses.First(s => s.Code == ApplicationConstants.ApplicationStatuses.CertificateForDelivery);
                 var statusCertificatePaperprint = statuses.First(s => s.Code == ApplicationConstants.ApplicationStatuses.CertificatePaperPrint);
                 var statusCertificateDelivered = statuses.First(s => s.Code == ApplicationConstants.ApplicationStatuses.Delivered);
-
-                var webPortalUrl = await _certificateService.GetWebPortalAddress();
-              
-                //todo: get mail data
-                var sysParams = await _dbContext.GSystemParameters.Where(s => s.Code == SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME).ToListAsync();
-                if (sysParams.Count != 1 || sysParams.Any(x=>string.IsNullOrEmpty(x.ValueString)))
-                {
-                    throw new Exception($"System parameters {SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME} are not set.");
-                }
                 string mailSubjectTemplate = MailResources.DELIVERY_MAIL_SUBJECT;
                 string mailBodyTemplate = MailResources.DELIVERY_MAIL_BODY;
-                string signingCertificateName = sysParams.First(s => s.Code == SystemParametersConstants.SystemParametersNames.SYSTEM_SIGNING_CERTIFICATE_NAME).ValueString;
+                var webPortalUrl = await _certificateService.GetWebPortalAddress();
+
+
                 foreach (IBaseIdEntity entity in entities)
                 {
                     numberOfProcesedEntities++;
                     try
                     {
-                        var certificate = (ACertificate)entity;
-                       var file =  await _certificateService.CreateCertificate(certificate, mailSubjectTemplate, mailBodyTemplate, signingCertificateName, 
-                                                                    statusCertificateServerSign, statusForDelivery, statusCertificateDelivered, statusCertificatePaperprint,   webPortalUrl);
-                        await _dbContext.SaveChangesAsync();
-                      //  System.IO.File.WriteAllBytes($"certificate_{certificate.Id}.pdf", file);
+                        var cert = (ACertificate)entity;
+                        //не обработваме справки
+                        //if (application.ApplicationType.Code == ApplicationConstants.ApplicationTypes.ConvictionRequest)
+                        //{
+                        //    var report = await _reportService.GenerateReportFromApplication(application, applicationStatus, (int)certificateValidityMonths);
+                        //    await _dbContext.SaveChangesAsync();
+                        //    var file = await _reportGenerationService.CreateReport(report, signingCertificateName);                      
+                        //    await _dbContext.SaveChangesAsync();
+                        //    //System.IO.File.WriteAllBytes($"hello{application.Id}.pdf", file);
+                        //}
+                        //else
+                        //{
+                        //_logger.LogTrace($"{application.Id} : Before GenerateCertificateFromApplication.");
+                        //var cert = await _applicationService.GenerateCertificateFromApplication(application, applicationStatus, bulletinCheckStatus, certificateContentReadyStatus, (int)certificateValidityMonths);
+                        //_logger.LogTrace($"{application.Id} : After GenerateCertificateFromApplication.");
+                        //await _dbContext.SaveChangesAsync();
+                        //_logger.LogTrace($"{application.Id} : After SaveChanges.");
+                        //_dbContext.ChangeTracker.Clear();
+                        if (cert.StatusCode == ApplicationConstants.ApplicationStatuses.CertificateContentReady && application.PurposeNavigation.ForSecondSignature.Value != true)
+                        {
+                            //създаване на pdf и завършване на процеса
+                            _logger.LogTrace($"{cert.Id} : Before CreateCertificate.");
+                            var file = await _certificateService.CreateCertificate(cert, mailSubjectTemplate, mailBodyTemplate, signingCertificateName,
+                                                                         statusCertificateUserSign, statusForDelivery, statusCertificateDelivered, statusCertificatePaperprint, webPortalUrl);
+                            _logger.LogTrace($"{cert.Id} : Before SaveChangesAsync.");
+                            await _dbContext.SaveChangesAsync();
+                            _logger.LogTrace($"{cert.Id} : After SaveChangesAsync.");
+                            _dbContext.ChangeTracker.Clear();
+                        }
+                        //}
+
                         numberOfSuccessEntities++;
                     }
                     catch (Exception ex)
                     {
                         numberOfFailedEntities++;
-                        _logger.LogError($"CertificateID {entity.Id}: " + ex.Message, ex.Data, ex);
+                        _dbContext.ChangeTracker.Clear();
+                        _logger.LogError($"ApplicationID {entity.Id}: " + ex.Message, ex.Data, ex);
                     }
 
 
